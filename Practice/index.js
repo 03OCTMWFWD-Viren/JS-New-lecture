68a0bdbbd00e384e0f2ccaa8e4b14acfa66d09c2
03OCTMWFWD-Viren <karena.viren@gmail.com>
2023-02-06 14:46:15 +0530

    JS-New-lecture

--- Practice/index.js
+++ Practice/index.js
@@ -13,21 +13,54 @@
 // const : block-scoped ;
 
 
-var x = 20; //scope global 
-{
-    var x = 30; global
-    .console.log('value of x is ${x}'); //30
-}
-console.log('value of x is $ {x}'); //30//
+// var x = 20; //scope global 
+// {
+//     var x = 30; global
+//     .console.log('value of x is ${x}'); //30
+// }
+// console.log('value of x is $ {x}'); //30//
 
-let a = 30; //global
-{
-    let a = 40; //block-scoped
-    console.log(a); //40
-}
-console.log(a);
-const p = 33; //global
-{
-    const p = 44; //local
-    console.log(p);
-}
+// let a = 30; //global/
+// let t = 45;
+// console.log(typeof(t));
+// let t1 = 4.56;
+// console.log(typeof(t1));
+
+// let num1 = number (prompt("enter the number:"));
+// let num2 = number (prompt("enter second nuber:"));
+
+// let sum = num1 + num2 ;
+// console.log(sum);
+// console.log(typeof(num1));
+
+//  example  
+
+" si = p*r*t*/100 " 
+" 25 - square root"
+
+let p = parseFloat(prompt("enter the principal amount:"));
+let r = parseFloat(prompt("enter the rate of intrest:"));
+let t = parseFloat(prompt("enter the duration"));
+
+let simple_interst = p*r*t*0.01;
+let amount = p+ simple_interst;
+console.log('simple interst of amount is ; ${simple_interst}');
+console.log(('total amount you will get $ {amount}'));
+
+let x = parseFloat(prompt("enter the number:"));
+console.log(x);
+
+// let x = parseFloat(prompt("enter the number:"));
+// console.log(x);
+
+// let x = number(prompt("enter the number:"));
+// console.log(x);
+
+// let x = "hello world";
+// if (is NaN(x))
+// {
+//     console.log("it is string");
+// }
+// else{
+//     console.log("it is number");
+// }
