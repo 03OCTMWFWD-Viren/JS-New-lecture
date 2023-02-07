dbbc97afb5f6a5407a468792a1073c71c83d3952
03OCTMWFWD-Viren <karena.viren@gmail.com>
2023-02-07 14:38:53 +0530

    JS-New-lecture

--- Practice/index.js
+++ Practice/index.js
@@ -15,8 +15,8 @@
 
 // var x = 20; //scope global 
 // {
-//     var x = 30; global
-//     .console.log('value of x is ${x}'); //30
+//     var x = 30;  //global
+//     console.log('value of x is ${x}'); //30
 // }
 // console.log('value of x is $ {x}'); //30//
 
@@ -26,38 +26,39 @@
 // let t1 = 4.56;
 // console.log(typeof(t1));
 
-// let num1 = number (prompt("enter the number:"));
-// let num2 = number (prompt("enter second nuber:"));
+// let num1 = Number(prompt("enter the number:"));
+// let num2 = Number(prompt("enter second nuber:"));
 
 // let sum = num1 + num2 ;
 // console.log(sum);
 // console.log(typeof(num1));
 
-//  example  
+//  example   //
 
-" si = p*r*t*/100 " 
-" 25 - square root"
+// " si = p*r*t*/100 " 
+// " 25 - square root"
 
-let p = parseFloat(prompt("enter the principal amount:"));
-let r = parseFloat(prompt("enter the rate of intrest:"));
-let t = parseFloat(prompt("enter the duration"));
+// let p = parseFloat(prompt("enter the principal amount:"));
+// let r = parseFloat(prompt("enter the rate of intrest:"));
+// let t = parseFloat(prompt("enter the duration"));
 
-let simple_interst = p*r*t*0.01;
-let amount = p+ simple_interst;
-console.log('simple interst of amount is ; ${simple_interst}');
-console.log(('total amount you will get $ {amount}'));
-
-let x = parseFloat(prompt("enter the number:"));
-console.log(x);
+// let simple_interst = p*r*t*0.01;
+// let amount = p+ simple_interst; 
+// console.log(`simple interst of amount is ; ${simple_interst}`);
+// console.log(`total amount you will get ${amount}`);
 
 // let x = parseFloat(prompt("enter the number:"));
+// console.log(x);
+
+// let x = parseInt(prompt("enter the number:"));
 // console.log(x);
 
 // let x = number(prompt("enter the number:"));
 // console.log(x);
 
 // let x = "hello world";
-// if (is NaN(x))
+// let x = 8;
+// if (isNaN(x))
 // {
 //     console.log("it is string");
 // }
