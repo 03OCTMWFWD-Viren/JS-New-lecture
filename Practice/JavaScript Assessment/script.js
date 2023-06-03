e0360ecdd50632fdba58abb652e781073fb6ae4b
03OCTMWFWD-Viren <karena.viren@gmail.com>
2023-06-03 14:09:43 +0530

    JS-New-lecture

---
+++ Practice/JavaScript Assessment/script.js
@@ -0,0 +1,6 @@
+// Question:-
+
+// myFunction([1,2,2,3])      Expected {1:1, 2:2, 3:1}
+// myFunction([9,9,9,99])      Expected {9:3, 99:1}
+// myFunction([4, 3, 2, 1])      Expected {1:1, 2:1, 3:1, 4:1}
+
